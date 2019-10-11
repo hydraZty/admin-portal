787c110aea5b3118188772d047f7748a9bba06a1
Samuel <lbh@decentfox.com>
2019-10-11 11:05:51 +0800

    fixed #14320 list all users with pagination

--- src/components/data-table/DataTable.js
+++ src/components/data-table/DataTable.js
@@ -32,19 +32,19 @@ const columns = [{
     <div className="col-policies">
       {
         policies.map(policy => (
-          <div key={policy.id} style={{ lineHeight: 2.1 }}>
-            <span style={{ marginRight: 6 }}>{policy.roleName}</span>
-            {policy.resources.slice(0, 2).map(resource => (
+          <div key={policy.role} style={{ lineHeight: 2.1 }}>
+            <span style={{ marginRight: 6 }}>{policy.role}</span>
+            {policy.resources && policy.resources.slice(0, 2).map(resource => (
               <Tag
                 color="#e7e7e7"
-                key={resource}
+                key={resource.policy}
                 style={{ color: '#3283c8', marginRight: 4 }}
               >
-                {resource}
+                {resource.resource}
               </Tag>
             ))}
             <span>
-              {policy.resources.length > 2 ? (
+              {policy.resources && policy.resources.length > 2 ? (
                 <Tag color="#e7e7e7" style={{ color: '#606060' }}>
                   {policy.resources.length - 2} more
                 </Tag>
--- src/components/layout/TableContent.js
+++ src/components/layout/TableContent.js
@@ -1,52 +1,123 @@
 import React, { PureComponent } from 'react';
 import { Col, Pagination, Row } from 'antd';
+import { find } from 'lodash';
 
 import DataTable from '../data-table/DataTable';
 import RowDetail from '../data-table/RowDetail';
 import './TableContent.less';
+import { arborist } from '../../utils/API';
 
-const data = [{
-  key: 1,
-  name: 'Daisy Dai',
-  email: 'daichen.daisy@gmail.com',
-  createdAt: '2019-09-07 15:42',
-  groups: ['CTDS-Mgmt'],
-  policies: [{
-    id: 1,
-    roleName: 'Admin',
-    resources: [],
-  }, {
-    id: 2,
-    roleName: 'Downloader',
-    resources: ['TCGA-BRCA', 'TCGA-LUAD', 'TCGA-BGM', 'TCGA-SKD'],
-  }, {
-    id: 3,
-    roleName: 'Workspace-user',
-    resources: ['TCGA-BRCA'],
-  }],
-}, {
-  key: 2,
-  name: 'Xenia Lu',
-  email: 'xenia.lyy@gmail.com',
-  createdAt: '2019-09-06 11:19',
-  groups: ['CTDS-Dev', 'NIAID-Dev'],
-  policies: [{
-    id: 1,
-    roleName: 'Admin',
-    resources: [],
-  }, {
-    id: 2,
-    roleName: 'Downloader',
-    resources: ['TCGA-BRCA', 'TCGA-LUAD', 'TCGA-BGM'],
-  }],
-}];
+// const data = [{
+//   key: 1,
+//   name: 'Daisy Dai',
+//   email: 'daichen.daisy@gmail.com',
+//   createdAt: '2019-09-07 15:42',
+//   groups: ['CTDS-Mgmt'],
+//   policies: [{
+//     id: 1,
+//     roleName: 'Admin',
+//     resources: [],
+//   }, {
+//     id: 2,
+//     roleName: 'Downloader',
+//     resources: ['TCGA-BRCA', 'TCGA-LUAD', 'TCGA-BGM', 'TCGA-SKD'],
+//   }, {
+//     id: 3,
+//     roleName: 'Workspace-user',
+//     resources: ['TCGA-BRCA'],
+//   }],
+// }, {
+//   key: 2,
+//   name: 'Xenia Lu',
+//   email: 'xenia.lyy@gmail.com',
+//   createdAt: '2019-09-06 11:19',
+//   groups: ['CTDS-Dev', 'NIAID-Dev'],
+//   policies: [{
+//     id: 1,
+//     roleName: 'Admin',
+//     resources: [],
+//   }, {
+//     id: 2,
+//     roleName: 'Downloader',
+//     resources: ['TCGA-BRCA', 'TCGA-LUAD', 'TCGA-BGM'],
+//   }],
+// }];
 
 class TableContent extends PureComponent {
   constructor (props) {
     super(props);
     this.state = {
-      selectedUser: data[0],
+      selectedUser: null,
+      pagination: {
+        page: 1,
+        page_size: 10,
+        total_count: 0,
+      },
+      users: [],
     };
+  }
+
+  componentDidMount = async () => {
+    await this.loadUsers();
+  }
+
+  loadUsers = async () => {
+    // eslint-disable-next-line camelcase
+    const { page, page_size } = this.state.pagination;
+    // eslint-disable-next-line camelcase
+    const resp = await arborist.get(`/user?page=${page}&page_size=${page_size}`);
+    const users = resp.users.map(user => {
+      const copiedUser = Object.assign(user);
+      const policies = [];
+      copiedUser.policies.forEach(policy => {
+        const currentPolicy = find(policies, d => d.role === policy.role);
+        if (currentPolicy) {
+          currentPolicy.resources.push({
+            resource: policy.resource,
+            policy: policy.policy,
+            expires_at: policy.expires_at,
+          });
+        } else {
+          policies.push({
+            role: policy.role,
+            resources: [{
+              resource: policy.resource,
+              policy: policy.policy,
+              expires_at: policy.expires_at,
+            }],
+          });
+        }
+      });
+      copiedUser.policies = policies;
+      return copiedUser;
+    });
+    await this.setState({
+      pagination: resp.pagination,
+      users,
+      selectedUser: resp.users[0],
+    });
+  }
+
+  onChangePage = async (page, pageSize) => {
+    await this.setState(prevState => ({
+      pagination: {
+        ...prevState.pagination,
+        page,
+        page_size: pageSize,
+      },
+    }));
+    await this.loadUsers();
+  }
+
+  onShowSizeChange = async (current, size) => {
+    await this.setState(prevState => ({
+      pagination: {
+        ...prevState.pagination,
+        page: current,
+        page_size: size,
+      },
+    }));
+    await this.loadUsers();
   }
 
   showUserDetail = (user) => {
@@ -61,17 +132,21 @@ class TableContent extends PureComponent {
         <Row>
           <Col span={14}>
             <DataTable
-              dataSource={data}
+              dataSource={this.state.users}
               onRowSelect={(user) => this.showUserDetail(user)}
             />
           </Col>
           <Col span={10}>
             <div className="table-pagination">
               <Pagination
+                pageSize={this.state.pagination.page_size}
+                current={this.state.pagination.page}
                 showSizeChanger
-                total={100}
+                total={this.state.pagination.total_count}
                 pageSizeOptions={['10', '25', '50']}
                 size="small"
+                onChange={this.onChangePage}
+                onShowSizeChange={this.onShowSizeChange}
               />
             </div>
             <RowDetail user={this.state.selectedUser} />
